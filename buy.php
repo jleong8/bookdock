2873df2d5c20fea0f5f7e6dae3aa9dc5905765c9
teamJackal <leongjason808@gmail.com>
2018-04-22 17:24:36 -1000

    Added image url

--- buy.php
+++ buy.php
@@ -18,9 +18,8 @@
 
 <?
 
-$keyword = 'nature';
-$url = "https://www.bing.com/images/search?q=".str_replace(" ", "+", $keyword)."&qs=n&form=QBIR&sp=-1&pq=".str_replace(" ", "+", $keyword)."&sc=8-34&sk=&cvid=0FB8E004AC034F21A51B1D59172B56A5";
-$output = get($url);
+
+
 // echo $output;
 
 function get($url) {
@@ -32,32 +31,39 @@ function get($url) {
   return $output;
 }
 
-preg_match_all('!<a class="thumb" target="_blank" href="(.*?)"!', $output, $url_matches);
-print_r($url_matches[1][0]);
 
-// echo "<div class=\"ui four cards\">";
-// foreach($result as $row) {
-// echo "<div class=\"card\">";
-// echo "<div class=\"image\">";
-// echo "<img src=\"https://vignette.wikia.nocookie.net/harrypotter/images/2/2c/Goblet_of_Fire_Film_Poster.jpg/revision/latest?cb=20140817011104\">";
-// echo  "</div>";
-// echo "<div class=\"content\">";
-// echo "<a class=\"header\">".$row['title']."</a>";
-// echo "<div class=\"meta\">";
-// echo "<span class=\"date\">Published: ".$row['year']."</span>";
-// echo "</div>";
-// echo "<div class=\"description\">";
-// echo "Author:   ";
-// echo $row['author'];
-// echo "</div>";
-// echo "</div>";
-// echo "<div class=\"extra content\">";
-// echo  "<a>";
-// echo "<i class=\"amazon icon\"></i>";
-// echo "<a href=\"http://www.amazon.com/s/?url=search-alias%3Daps&field-keywords=".$row['title']."&Go=Go\">Amazon</a>";
-// echo "</a>";
-// echo "</div>";
-// echo "</div>";
-// }
-// echo "</div>";
+
+echo "<div class=\"ui four cards\">";
+foreach($result as $row) {
+
+$keyword = $row['title'];
+$url = "https://www.bing.com/images/search?q=".str_replace(" ", "+", $keyword)."&qs=n&form=QBIR&sp=-1&pq=".str_replace(" ", "+", $keyword)."&sc=8-34&sk=&cvid=0FB8E004AC034F21A51B1D59172B56A5";
+$output = get($url);
+
+preg_match_all('!<a class="thumb" target="_blank" href="(.*?)"!', $output, $url_matches);
+//$print = print_r($url_matches[1][0]);
+
+echo "<div class=\"card\">";
+echo "<div class=\"image\">";
+echo "<img src=\"print_r($url_matches[1][0])"">";
+echo  "</div>";
+echo "<div class=\"content\">";
+echo "<a class=\"header\">".$row['title']."</a>";
+echo "<div class=\"meta\">";
+echo "<span class=\"date\">Published: ".$row['year']."</span>";
+echo "</div>";
+echo "<div class=\"description\">";
+echo "Author:   ";
+echo $row['author'];
+echo "</div>";
+echo "</div>";
+echo "<div class=\"extra content\">";
+echo  "<a>";
+echo "<i class=\"amazon icon\"></i>";
+echo "<a href=\"http://www.amazon.com/s/?url=search-alias%3Daps&field-keywords=".$row['title']."&Go=Go\">Amazon</a>";
+echo "</a>";
+echo "</div>";
+echo "</div>";
+}
+echo "</div>";
 ?>
