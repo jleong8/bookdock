23eb777c6661f6eebcb34adea4b5c3623d2af1ce
teamJackal <leongjason808@gmail.com>
2018-04-23 23:58:56 -1000

    'j'

--- buy.php
+++ buy.php
@@ -72,7 +72,7 @@ function get($url) {
 
 
 
-echo "<div class=\"ui four cards\">";
+echo "<div class=\"ui container\">";
 foreach($result as $row) {
 ?>
 <form action="buy.php?action=add&id=<? echo $row['book_id']?>" method="POST">
