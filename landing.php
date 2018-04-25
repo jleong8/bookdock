d195820291c6911d4bc160bfd1d72955f652c544
jleong8 <jleong8@hawaii.edu>
2018-04-24 23:33:33 -1000

    's'

--- landing.php
+++ landing.php
@@ -1,8 +1,21 @@
 <?php include 'header.php'; ?>
 
 
-<div class="ui column grid">
+<div class="ui column centered grid">
+<form class="ui form" action="sell.php" method="POST">
+  <div class="field">
+    <label>Title</label>
+    <input type="text" name="title" placeholder="Title">
+  </div>
+  <div class="field">
+    <label>Author</label>
+    <input type="text" name="author" placeholder="Author">
+  </div>
+    <div class="field">
+    <label>Year</label>
+    <input type="text" name="year" placeholder="Year">
+  </div>
 
-    <label>Dashboard</label>
-
+  <button class="ui button" name="submit" id="submit" type="submit">Submit</button>
+</form>
 </div>
