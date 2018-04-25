85abe87e15d9ffdb89990ad2d5d07c5d79fcabc5
jleong8 <jleong8@hawaii.edu>
2018-04-24 23:56:20 -1000

    changed colors

--- landing.php
+++ landing.php
@@ -11,15 +11,15 @@
 
 <div class="ui column centered grid">
 <h1>Dashboard</h1>
-<hr style="height:5px;border:none;color:#333;background-color:#333;" />
+<hr style="height:5px;border:none;color:#7FFFD4;background-color:#7FFFD4;" />
 <h1>Wishlist</h1>
 <br>
-<hr style="height:5px;border:none;color:#333;background-color:#333;" />
+<hr style="height:5px;border:none;color:#7FFFD4;background-color:#7FFFD4;" />
 <h1>Sold books</h1>
 <br>
-<hr style="height:5px;border:none;color:#333;background-color:#333;" />
+<hr style="height:5px;border:none;color:#7FFFD4;background-color:#7FFFD4;" />
 <h1>Messages</h1>
 <br>
-<hr style="height:5px;border:none;color:#333;background-color:#333;" />
+<hr style="height:5px;border:none;color:#7FFFD4;background-color:#7FFFD4;" />
 
 </div>
