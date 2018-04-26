c4ab7469e226866dfc35e07144593a314af4b569
jleong8 <jleong8@hawaii.edu>
2018-04-25 23:38:10 -1000

    huh

--- about_us.php
+++ about_us.php
@@ -2,9 +2,10 @@
 <?php include('connect.php'); ?>
 
 <div class="ui column centered grid">
+<iframe id="ytplayer" type="text/html" width="640" height="360"
+src="https://www.youtube.com/watch?v=EtK0D-jy08s"
+frameborder="0"></iframe>
 <h1>About Us</h1>
-<iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY">
-</iframe>
 <h3>Book Dock is a website that will provide UH students a platform to exchange and sell their books.</h3>
 <h3>Created by Jason Leong</h3>
 
