edb8cc02d370878b5fe9cb19919273b34ed6fc68
jleong8 <jleong8@hawaii.edu>
2018-04-25 23:55:21 -1000

    s

--- about_us.php
+++ about_us.php
@@ -10,13 +10,12 @@
   }
   iframe {
     margin-top: 100px;
-    text-align: center;
   }
 </style>
 
 
 
-<iframe width="560" height="315"  src="https://www.youtube.com/embed/EtK0D-jy08s" frameborder="0" allowfullscreen></iframe>
+<div align="center"><iframe width="560" height="315"  src="https://www.youtube.com/embed/EtK0D-jy08s" frameborder="0" allowfullscreen></iframe></div>
 <h1>About Us</h1>
 <h3>Book Dock is a website that will provide UH students a platform to exchange and sell their books.</h3>
 <h3>Created by Jason Leong</h3>
