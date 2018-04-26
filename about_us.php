af8563d9e62f46fa40fad0c30a50809092b825e4
jleong8 <jleong8@hawaii.edu>
2018-04-25 23:43:17 -1000

    's'

--- about_us.php
+++ about_us.php
@@ -2,7 +2,7 @@
 <?php include('connect.php'); ?>
 
 <div class="ui column centered grid">
-<iframe width="640" height="360" src="https://www.youtube.com/embed/EtK0D-jy08s" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<iframe width="560" height="315"  src="https://www.youtube.com/embed/EtK0D-jy08s" frameborder="0" allowfullscreen></iframe>
 <br>
 <h1>About Us</h1>
 <h3>Book Dock is a website that will provide UH students a platform to exchange and sell their books.</h3>
