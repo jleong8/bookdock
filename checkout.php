239bea252fb303a1aaa01ff3574ebf7101c72d31
jleong8 <jleong8@hawaii.edu>
2018-05-02 23:36:08 -1000

    s

--- checkout.php
+++ checkout.php
@@ -23,7 +23,7 @@ session_start();
     input[type=submit] {
       float: right;
       margin-bottom: 2px;
-      margin-right: 2px;
+      margin-right: 10px;
     }
 </style>
 
@@ -49,11 +49,9 @@ function get($url) {
 }
 
 ?>
-<div>
-<input type="submit" id="checkout" value="Checkout">
-</div>
 
 <div class="ui four cards">
+<input type="submit" id="checkout" value="Checkout">
 <?php
 foreach($result as $row) {
 ?>
