87d92984006ea1a3a9ca8d1c5eb017cd37eb340b
jleong8 <jleong8@hawaii.edu>
2018-05-02 23:13:47 -1000

    s

--- remove.php
+++ remove.php
@@ -6,7 +6,7 @@ session_start();
 <?php
 
 $buyer_id = $_GET['id'];
-$sql = "UPDATE `books` SET `book_id` = '""' WHERE `buyer_id` = '".$_SESSION['uid']."'";
+$sql = "UPDATE `books` SET `book_id` = '". ."' WHERE `buyer_id` = '".$buyer_id."'";
 //echo $sql;
 $sel = $pdo->prepare($sql);
 $sel->execute();
